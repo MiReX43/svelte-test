440fb59ef564f21eec0a6e4a74960fb9163d9969
Denis <den.girevoi_43@mail.ru>
2024-02-28 15:10:18 +0300

    add main page and nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+
+<nav>
+    <div class="nav-container">
+    <a href="/" class="nav-logo" title="Back to Homepage">Денис Гиревой</a>
+    <div class="nav-links">
+    <a href="/about" class="link">Обо мнe</a> 
+    <a href="/blog" class="link">Блoг</a>
+    <a href="/projects" class="link">Пopтфолио</a>
+    <a href="/contact" class="link">Kонтакты</a>
+    </div>
+    </div>
+</nav>
+
+    <div class="container">
+    <!-- Pages will be injected below -->
+    <slot></slot>
+    </div>
+
+<style>
+    .container {
+    max-width: 1400px;
+    margin: 50px auto;
+    }
+</style>
